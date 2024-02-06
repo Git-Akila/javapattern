b8a68c4eb688a41fabe53a6dd4c763373c2d9a5c
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2024-02-06 14:32:14 +0530

    Create diamond.java
       *
     ***
    *****
     ***
      *

---
+++ diamond.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class pattern1 {
+    public static void main(String args[]){
+        Scanner aki=new Scanner(System.in);
+        System.out.println("Enter the size:");
+        int a=aki.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=i;j<=a;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<a;j++){
+                System.out.print("*");
+            }
+
+            for(int j=i;j<=a;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
