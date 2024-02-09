294abb91e637d2b07ae3d20001ba1f077c47e5dd
Git-Akila <118921510+Git-Akila@users.noreply.github.com>
2024-02-09 18:08:21 +0530

    Create pyTriangle.java
              *
            * * *
          * * * * *

---
+++ pyTriangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class pattern1{
+public static void main(String args[]) {
+    Scanner aki=new Scanner(System.in);
+    System.out.println("Enter the Size:");
+    int a=aki.nextInt();int z=1;
+    for(int i=1;i<=a;i++) {
+        for (int j = a-1; j >=i; j--) {
+
+            System.out.print(" ");
+
+        }for(int k=1;k<=z;k++){
+            System.out.print("*");
+        }z+=2;
+        System.out.println();
+        }
+    }
+}
